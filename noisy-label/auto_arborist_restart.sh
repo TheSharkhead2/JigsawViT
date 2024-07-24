8afd30b4a1275ceaf3d481c0f49d6d45c8f80f9d
Theo Rode <theorodester@gmail.com>
2024-07-24 15:12:42 -0700

    resume from path

---
+++ noisy-label/auto_arborist_restart.sh
@@ -0,0 +1,23 @@
+CUDA_VISIBLE_DEVICES=2,3 python -m torch.distributed.launch --nproc_per_node=2 --use_env --master_port=29602 jigsaw-vit/main.py \
+  --arch 'vit_small_patch16' \
+   --input-size 224 \
+   --batch-size 128 \
+   --max-lr 1e-3 \
+   --min-lr 1e-6 \
+   --total-iter 400000 \
+   --warmup-iter 20000 \
+   --niter-eval 10000 \
+   --weight-decay 0.05 \
+   --smoothing 0.1 \
+   --mixup 0.8 \
+   --cutmix 1.0 \
+   --world-size 2 \
+   --workers 16 \
+   --rank 0 \
+   --data-path /home/sandeepmukh/data/autoarborist \
+   --data-set 'auto_arborist' \
+   --eta 2.0 \
+   --mask-ratio 0.1 \
+   --out-dir /home/trode/jigsaw-vit-noisy/out_aa \
+   --resumePth /home/trode/jigsaw-vit-noisy/out_aa/netBest.pth \
+   --wandb-project autoarborist_theo
